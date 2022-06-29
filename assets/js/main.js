8a795c2f788091e21078a8e438269c9432b6eeec
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-06-29 14:18:12 +0200

    added else if for no splitting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 let tippText = document.querySelector("p#calculatedTipp")
 let tippTextH2 = document.querySelector("h2#calculatedTipp")
 let totalText = document.querySelector("p#calculatedTotal")
+let totalTextH2 = document.querySelector("h2#calculatedTotal")
 let splittText = document.querySelector("p#calculatedSplitt")
+let splittTextH2 = document.querySelector("h2#calculatedSplitt")
 
 countAmounts = (event) =>{
     event.preventDefault();
@@ -19,16 +21,30 @@ countAmounts = (event) =>{
     let endNumEur = (splittNumFixed * peopleNum).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
 
     if (amountNum == 0 || peopleNum == 0){
+        tippTextH2.innerText = `Calculated tip:`
         tippText.innerText = `Nothing set yet`;
+        totalTextH2.innerText = `Total Amount:`
         totalText.innerText =  `Nothing set yet`;
+        splittTextH2.innerText = `Price per person:`
         splittText.innerText =  `Nothing set yet`;
+        infoText.innerText = "Please make sure to provide information in each field!";
+    }
+    else if (peopleNum == 1){
+        tippTextH2.innerText = `${qualityNum}% tip:`
+        tippText.innerText = `${tippNumEur}`;
+        totalTextH2.innerText = `Total Amount:`
+        totalText.innerText = `${totalNumEur}`;
+        splittTextH2.innerText = "";
+        splittText.innerText = "";
         infoText.innerText = "";
     }
     else{
         tippTextH2.innerText = `${qualityNum}% tip:`
         tippText.innerText = `${tippNumEur}`;
+        totalTextH2.innerText = `Total Amount:`
         totalText.innerText = `${totalNumEur}`;
+        splittTextH2.innerText = `Price per person*:`
         splittText.innerText = `${splittNumEurRounded},00 €`;
-        infoText.innerText = `*Since we don't like small change, the price per person is rounded. So the service gets a total of ${endNumEur}. if you don't see it that way, simply split the bill into ${splittNumEur} each.`;
+        infoText.innerText = `*Since we don't like small change, the price per person is rounded. So the service gets a total of ${endNumEur}. If you don't see it that way, simply split the bill into ${splittNumEur} each.`;
     }
 }
