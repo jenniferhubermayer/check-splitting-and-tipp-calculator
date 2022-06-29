a0c60e154c46452849c8038428b2d67a70154bc0
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-06-29 13:43:11 +0200

    CSS changes for the output, JS changes for better results in €

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 let tippText = document.querySelector("p#calculatedTipp")
+let tippTextH2 = document.querySelector("h2#calculatedTipp")
 let totalText = document.querySelector("p#calculatedTotal")
 let splittText = document.querySelector("p#calculatedSplitt")
 
@@ -12,9 +13,11 @@ countAmounts = (event) =>{
     let tippNumEur = ((qualityNum * amountNum) / 100).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
     let totalNum = +amountNum + +tippNum;
     let totalNumEur = (+amountNum + +tippNum).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
-    let splittNum = (totalNum / peopleNum).toFixed();
-    let endNum = splittNum * peopleNum;
-    let splittUglyNumEur = (totalNum / peopleNum).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
+    let splittNumFixed = (totalNum / peopleNum).toFixed();
+    let splittNumEur = (totalNum / peopleNum).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
+    let splittNumEurRounded = (totalNum / peopleNum).toLocaleString('de-DE', {maximumFractionDigits:'0'});
+    let endNumEur = (splittNumFixed * peopleNum).toLocaleString('de-DE', {style: 'currency', currency: 'EUR' });
+
     if (amountNum == 0 || peopleNum == 0){
         tippText.innerText = `Nothing set yet`;
         totalText.innerText =  `Nothing set yet`;
@@ -22,9 +25,10 @@ countAmounts = (event) =>{
         infoText.innerText = "";
     }
     else{
-        tippText.innerText = `${tippNumEur} (${qualityNum}% of your checks amount)`;
+        tippTextH2.innerText = `${qualityNum}% tip:`
+        tippText.innerText = `${tippNumEur}`;
         totalText.innerText = `${totalNumEur}`;
-        splittText.innerText = `${splittNum},00 € (rounded*)`;
-        infoText.innerText = `*Since we don't like small change, the price per person is rounded. So the service gets a total of ${endNum} €. if you don't see it that way, simply split the bill into ${splittUglyNumEur} each.`;
+        splittText.innerText = `${splittNumEurRounded},00 €`;
+        infoText.innerText = `*Since we don't like small change, the price per person is rounded. So the service gets a total of ${endNumEur}. if you don't see it that way, simply split the bill into ${splittNumEur} each.`;
     }
 }
